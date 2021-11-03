bdc481a1a0d3c556cbecb24f56252edf4f267b88
Max Wolf <maxcharleswolf@gmail.com>
2021-11-03 14:05:26 -0600

    fix: fixed app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import { App } from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const searchText = screen.getByText(/Search/i)
+  const searchText = screen.getByText(/RepoSearch/i)
   expect(searchText).toBeInTheDocument()
 })
